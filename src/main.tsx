c0f71c724dbf4a911a9c7a11de31219ad995e18e
Cosmas Okoth <cosmasokoth18@gmail.com>
2022-11-10 13:24:44 +0300

    Updated

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,16 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
+window.onload = function () {
+    setTimeout(function () {
+
+        alert('5 minutes later, ready to initialize');
+
+        ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+            <React.StrictMode>
+                <App/>
+            </React.StrictMode>
+        );
+
+    }, 5 * 60 * 1000);
+};
